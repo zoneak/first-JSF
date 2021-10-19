4650f0a2899578675e57661a61b58a92d7161855
Adriano Kaito <adriano.kaito@gmail.com>
2021-10-19 15:05:25 -0300

    Complemento datatables com elementos

--- src/main/java/com/firstjsf/bean/ConsultaClienteBean.java
+++ src/main/java/com/firstjsf/bean/ConsultaClienteBean.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import com.firstjsf.domain.Cliente;
 
 @ManagedBean
+@ViewScoped
 public class ConsultaClienteBean {
 
 	private List<Cliente> clientes = new ArrayList<Cliente>();
@@ -17,6 +19,12 @@ public class ConsultaClienteBean {
 		this.getClientes().add(new Cliente(2, "Shinji Ikari", "Chiba"));
 		this.getClientes().add(new Cliente(4, "Asuka Langley", "Nagoya"));
 		this.getClientes().add(new Cliente(6, "Misato Katsuragi", "Osaka"));
+	}
+	
+	public void salvar() {
+		for (Cliente c : this.getClientes()) {
+			System.out.println(c.getCodigo() + " - " + c.getNome());
+		}
 	}
 	
 	public List<Cliente> getClientes() {
